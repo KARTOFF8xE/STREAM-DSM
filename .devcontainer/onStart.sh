587065554ed85b35b5dc31e61784ab7e8ff301e4
KARTOFF8xE <georgmuck1@gmail.com>
2025-01-10 12:01:38 +0000

    created devcontainer and added .gitignore

---
+++ .devcontainer/onStart.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+declare -a sources=(
+    "/opt/ros/humble/setup.zsh"
+    "/workspaces/DiplArbeitContainer/ws/cpp_pubsub/install/setup.zsh"
+    "/workspaces/DiplArbeitContainer/ws/ros2_tracing/install/setup.zsh"
+    )
+
+for source in "${sources[@]}"
+do
+    echo "source $source" | sudo tee -a ~/.zshrc > /dev/null
+done
