04493ed6dba772e969cc7123dfc6014a16a382f1
KARTOFF8xE <georgmuck1@gmail.com>
2025-01-18 09:20:45 +0000

    updated .gitignore and added source-component to graph

--- ws/src/tracer/src/babelTrace.c
+++ ws/src/tracer/src/babelTrace.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 int main() {
+    printf("Load Plugin\n");
     const bt_plugin *plugin;
-    bt_plugin_find_status status = bt_plugin_find(
+    bt_plugin_find_status plugin_find_status = bt_plugin_find(
         "ctf",
         BT_FALSE,
         BT_FALSE,
@@ -11,43 +12,72 @@ int main() {
         BT_TRUE,
         BT_TRUE,
         &plugin);
-
-    if (status == BT_PLUGIN_FIND_STATUS_OK) {
-        printf("Success.\n");
-    }
-    if (status == BT_PLUGIN_FIND_STATUS_NOT_FOUND) {
-        printf("Plugin not found.\n");
-    }
-    if (status == BT_PLUGIN_FIND_STATUS_MEMORY_ERROR) {
-        printf("Out of memory.\n");
-    }
-    if (status == BT_PLUGIN_FIND_STATUS_ERROR) {
-        printf("Error.\n");
+    switch (plugin_find_status) {
+        case BT_PLUGIN_FIND_STATUS_OK: printf("\tSuccess.\n"); break;
+        case BT_PLUGIN_FIND_STATUS_NOT_FOUND: fprintf(stderr, "\tPlugin not found.\n"); break;
+        case BT_PLUGIN_FIND_STATUS_MEMORY_ERROR: fprintf(stderr, "\tOut of memory.\n"); break;
+        case BT_PLUGIN_FIND_STATUS_ERROR: fprintf(stderr, "\tError.\n"); break;
+        default: printf("\tHopefully never reached\n");
     }
 
-    bt_graph *graph = NULL;
-    const char *url = "net://127.0.0.1"; // URL des Relay-Daemons
-    bt_value *params = NULL;
+    const bt_component_class_source *component_class_source;
+    component_class_source = bt_plugin_borrow_source_component_class_by_name_const(plugin, "lttng-live");
 
-    // Graph erstellen
-    graph = bt_graph_create(0);
+    printf("Create Array value\n");
+    bt_value *array_value = bt_value_array_create();
+    bt_value_array_append_element_status array_append_element_status = bt_value_array_append_string_element(array_value, "net://127.0.0.1");
+    switch (array_append_element_status) {
+        case BT_VALUE_ARRAY_APPEND_ELEMENT_STATUS_OK: printf("\tSuccess.\n"); break;
+        case BT_VALUE_ARRAY_APPEND_ELEMENT_STATUS_MEMORY_ERROR: fprintf(stderr, "\tOut of Memory.\n"); break;
+        default: printf("\tHopefully never reached\n");
+    }
+    // printf("Length of array: %ld\n", bt_value_array_get_length(array_value));
+
+    printf("Create Map value\n");
+    bt_value *map_value = bt_value_map_create();
+    bt_value_map_insert_entry_status map_insert_entry_status = bt_value_map_insert_entry(map_value, "inputs", array_value);
+    switch (map_insert_entry_status) {
+        case BT_VALUE_MAP_INSERT_ENTRY_STATUS_OK: printf("\tSuccess.\n"); break;
+        case BT_VALUE_MAP_INSERT_ENTRY_STATUS_MEMORY_ERROR: fprintf(stderr, "\tOut of Memory.\n"); break;
+        default: printf("\tHopefully never reached\n");
+    }
+    // printf("Map has Entry 'inputs': %d\tSize of Map: %ld\n", bt_value_map_has_entry(map_value, "inputs"), bt_value_map_get_size(map_value));
+
+    printf("Create Graph\n");
+    bt_graph *graph = bt_graph_create(0);
     if (!graph) {
         fprintf(stderr, "Fehler: Graph konnte nicht erstellt werden.\n");
         return 1;
     }
 
-    if (bt_plugin_borrow_source_component_class_by_name_const(plugin, "lttng-live") != 0) {
-        printf("Some Error at bt_plugin_borrow_source_component_class_by_name_const.\n");
+    printf("Add Component to Graph\n");
+    const bt_component_source *component_source;
+    bt_graph_add_component_status add_source_component_status = bt_graph_add_source_component(
+        graph,
+        component_class_source,
+        "lttng-live-foo",
+        map_value,
+        BT_LOGGING_LEVEL_TRACE,
+        &component_source
+    );
+    switch (add_source_component_status) {
+        case BT_GRAPH_ADD_COMPONENT_STATUS_OK: printf("\tSuccess.\n"); break;
+        case BT_GRAPH_ADD_COMPONENT_STATUS_MEMORY_ERROR: fprintf(stderr, "Out of Memory.\n"); break;
+        case BT_GRAPH_ADD_COMPONENT_STATUS_ERROR: fprintf(stderr, "\tOther error.\n"); break;
+        default: printf("\tHopefully never reached\n");
     }
     
-    // Parameter setzen (Relay-Daemon URL)
-    params = bt_value_map_create();
-    if (!params) {
-        fprintf(stderr, "Fehler: Parameter-Map konnte nicht erstellt werden.\n");
-        bt_plugin_put_ref(plugin);
-        bt_graph_put_ref(graph);
-        return 2;
-    }
+    printf("Number of Ports the Source Component has: %ld\n", bt_component_source_get_output_port_count(component_source));
+    //-------------------------------
 
-    bt_value_map_insert_string_entry(params, "url", url);
+    
+
+
+
+
+    // printf("Run Graph\n");
+    // bt_graph_run_status graph_status = bt_graph_run(graph);
+    // if (graph_status != 0) {
+    //     fprintf(stderr, "\tsome error accured on running graph");
+    // }
 }
