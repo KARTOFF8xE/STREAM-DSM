1fbb0000da6c065f6dbdb07cdc34b6d8c8138a5d
KARTOFF8xE <georgmuck1@gmail.com>
2025-01-23 16:12:44 +0000

    updated WS-buildUp

--- .devcontainer/onCreate.zsh
+++ .devcontainer/onCreate.zsh
@@ -1,11 +1,12 @@
 #!/bin/zsh
 
-# ./.devcontainer/fetchFastDDS.zsh
-# ./.devcontainer/fetchROS2Tracing.zsh
-# ./.devcontainer/fetchAndBuildBabelTrace.zsh
+for file in ./.devcontainer/onCreate/*.zsh
+do
+    echo "\e[34;1mexecuting ./$file\e[0m"
+    ./$file
+done
 
-./.devcontainer/onCreate/*.zsh
-
+# ./.devcontainer/onCreate/setupEverything.zsh
 cd /workspaces/DiplArbeitContainer/ws
 source /opt/ros/humble/setup.zsh
 colcon build --cmake-args -DFASTDDS_STATISTICS=ON --symlink-install
