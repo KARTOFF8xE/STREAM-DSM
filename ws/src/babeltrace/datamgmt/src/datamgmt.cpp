9b7eec9bff33f9424ccfe458793e7ca432151265
KARTOFF8xE <georgmuck1@gmail.com>
2025-02-12 07:54:35 +0000

    added todos

--- ws/src/babeltrace/datamgmt/src/datamgmt.cpp
+++ ws/src/babeltrace/datamgmt/src/datamgmt.cpp
@@ -3,28 +3,38 @@
 #include <thread>
 #include <chrono>
 #include <atomic>
+#include <vector>
 
-std::atomic<bool> runProcObserver;
+#include <ipc/ipc-server.hpp>
 
-void procObserver() {
-    int counter = 0;
-    while (runProcObserver) {
-        std::cout << counter++ << std::endl;
+struct Client {
+    pid_t pid;
+    requestId_t requestId;
+};
+
+void procObserver(IpcServer server, Client client) {
+    std::vector<Client> procObserverClients;
+    procObserverClients.push_back(client);
+
+    // TODO gather a list with all Node-PIDs (neo4j query)
+    // TODO needs a communication with tracer (subscribe Events)
+
+    while (procObserverClients.size() > 0) {
+        // TODO receive event based node updates
+        // TODO receive information about eventual new clients (or unsubscriptions)
+        // TODO check if processes are still alive
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
+
+    // TODO unsubscribe Events
 }
 
 int main() {
-    runProcObserver = true;
-    std::thread procObserverThread(procObserver);
+    IpcServer server(0);
+    bool procObserverRunning = false;
 
-    for (int i = 0; i < 15; i++) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(400));
-        std::cout << "in main: " << i << std::endl;
+    while (true) {
+        // TODO receive different messages as a server (subscriptions and unsubscriptions)
+        // TODO handle threads that are running
     }
-
-    runProcObserver = false;
-    procObserverThread.join();
-    std::cout << "finished thread" << std::endl;
-
 }
