cffedc736968c51cbc803a1c89e4c5df364a07ae
KARTOFF8xE <georgmuck1@gmail.com>
2025-02-13 15:37:37 +0000

    added pipe module for better handling

--- ws/src/babeltrace/datamgmt/include/pipe/pipe.hpp
+++ ws/src/babeltrace/datamgmt/include/pipe/pipe.hpp
@@ -1,1 +1,27 @@
 #pragma once
+
+#include "datamgmt/common.hpp"
+
+/**
+ * @brief Transforms the payload into a message and writes it to the pipe.
+ * 
+ * @param __fd File descriptor for the write end of the pipe.
+ * @param payload The payload that shall be send to the pipe.
+ * @param __n Size of the message.
+ * 
+ * @return The number written. Or -1.
+ */
+template<typename T>
+ssize_t writeT(int __fd, const T payload, size_t __n = MSGSIZE);
+
+/**
+ * @brief Reads a message from the pipe and transforms it into the espected payload.
+ * 
+ * @param __fd File descriptor for the read end of the pipe.
+ * @param payload The payload that shall be read from the pipe.
+ * @param __n Size of the message.
+ * 
+ * @return The number read. -1 for errors or 0 for EOF.
+ */
+template<typename T>
+ssize_t readT(int __fd, const T &payload, size_t __n = MSGSIZE);
--- ws/src/babeltrace/datamgmt/src/pipe/pipe.cpp
+++ ws/src/babeltrace/datamgmt/src/pipe/pipe.cpp
@@ -1,15 +1,53 @@
 #include "pipe.hpp"
 #include "datamgmt/common.hpp"
+#include "util.hpp"
 
+#include <iostream>
 #include <unistd.h>
 #include <fcntl.h>
 
 int getPipe(int p[2], bool blocking = false) {
     if (int ret = pipe(p) != 0) {
+        std::cerr << "\e[31mError: Failed to create pipe. Code: " << ret << "\e[30m" << std::endl;
         return ret;
     }
 
     if (!blocking) {
-        int retval = fcntl( p[0], F_SETFL, fcntl(p[0], F_GETFL) | O_NONBLOCK);
+        int ret = fcntl( p[0], F_SETFL, fcntl(p[0], F_GETFL) | O_NONBLOCK);
+        if (ret != 0) {
+            std::cerr << "\e[31mError: Failed to set pipe not blocking. Code: " << ret << "\e[30m" << std::endl;
+            return ret;
+        }
     }
+
+    return 0;
 }
+
+template ssize_t writeT<Data>(int __fd, const Data payload, size_t __n = MSGSIZE);
+template ssize_t readT<Data>(int __fd, const Data &payload, size_t __n = MSGSIZE);
+
+template<typename T>
+ssize_t writeT(int __fd, const T payload, size_t __n = MSGSIZE) {
+    char *msg = serialize(payload);
+    
+    int ret = write(__fd, msg, __n);
+    if (ret == -1) {
+        std::cerr << "\e[31mError: Failed to write pipe.\e[30m" << std::endl;
+    }
+    
+    return ret;
+};
+
+template<typename T>
+ssize_t readT(int __fd, const T &payload, size_t __n = MSGSIZE) {
+    char *msg;
+    int ret = read(__fd, msg, __n);
+    if (ret == -1) {
+        std::cerr << "\e[31mError: Failed to read pipe.\e[30m" << std::endl;
+        return ret;
+    }
+
+    payload = deserialize(msg);
+
+    return 0;
+};
